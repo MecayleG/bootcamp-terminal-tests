7ab3f6673f682c54ecf24d1a56f27cd315176e8b
root <root@DESKTOP-V8P4AHJ.localdomain>
2020-08-05 12:33:51 +0200

    fixed countAllFromTown test

--- countAllFromTown.js
+++ countAllFromTown.js
@@ -1,21 +1,12 @@
-module.exports = function countAllFromTown(count){
+module.exports = function countAllFromTown(count, town){
   var towns = count.split(",")
   var isEqual = [];
-  var isEqual2 = [];
-  var isEqual3 = [];
   for(var i = 0; i < towns.length; i++){
-    var from = towns[i];
-    if(from.startsWith("CL")){
+    var from = towns[i].trim();
+    if(from.startsWith(town)){
       isEqual.push(from);
     }
-     else if(from.startsWith("CF")){
-      isEqual2.push(from);
-    }
-    else if(from.startsWith("CJ")){ 
-      isEqual3.push(from);
-    }
   }
-  console.log(isEqual.length)
-  console.log(isEqual2.length)
-  console.log(isEqual3.length)
+  return isEqual.length;
+  
 };
--- countAllPaarl.js
+++ countAllPaarl.js
@@ -1,7 +1,6 @@
 module.exports = function countAllPaarl(paarlReg){
   var paarl = paarlReg.split(",")
   var reg = [];
-  	console.log(paarl)
   for(var i = 0; i < paarl.length; i++){
     var num = paarl[i].trim();
     if(num.startsWith("CJ")){
